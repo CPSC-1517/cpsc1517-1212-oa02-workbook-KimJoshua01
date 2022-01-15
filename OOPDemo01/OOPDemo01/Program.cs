1ce6460c60341a6c7185e70680b8d4a0a5541b78
KimJoshua01 <90473470+KimJoshua01@users.noreply.github.com>
2022-01-14 17:17:54 -0700

    first commit

---
+++ OOPDemo01/OOPDemo01/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo01
+{
+    internal class Course
+    {
+        #region Readonly Data Fields
+        public readonly string CourseNo;
+        public readonly string CourseName;
+        public readonly List<string> Students = new List<string>();
+        #endregion
+
+        #region Readonly Property
+        public int StudentCount
+        {
+            get { return Students.Count; }
+        }
+        #endregion
+
+        #region Constructors
+        public Course(string courseNo, string courseName)
+        {
+            //validate that courseNo is not null,
+            //or an empty string
+            //and must contains exactly 8 characters
+            //where the first 4 characters are letters and the last 4 characters
+            //are digits
+            if (string.IsNullOrEmpty(courseNo))
+            {
+                throw new ArgumentNullException("CourseNo is required");
+            }
+            if (courseNo.Length != 8)
+            {
+                throw new ArgumentException("CourseNo contain exactly 8 characters.");
+            }
+
+
+            CourseNo = courseNo;
+
+            //validate that courseName is not null or an empty string
+            CourseName = courseName;
+        }
+        #endregion
+        #region Instance-Level Methods
+        public void AddStudent(string name)
+        {
+            Students.Add(name);
+        }
+        public void RemoveStudent(string name)
+        {
+            Students.Remove(name);
+        }
+        public bool SaveToFile(string filepath)
+        {
+            bool success = false;
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filepath))
+                {
+                    // Write the courseNo and courseName to the file
+                    writer.WriteLine(CourseNo);
+                    writer.WriteLine(CourseName);
+                    // Write the name of all the students in the course
+                    foreach (string student in Students)
+                    {
+                        writer.WriteLine(student);
+                    }
+                    success = true;
+                }
+            }
+            catch
+            {
+                success = false;
+            }
+
+            return success;
+        }
+        #endregion
+        public bool LoadFromFile(string filepath)
+        {
+            bool success = false;
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filepath))
+                {
+                    // Read the CourseNo and CourseName from the file
+                    var courseNo = reader.ReadLine();
+                    var courseName = reader.ReadLine();
+
+                    // Read the student names from the file
+                    while (reader.EndOfStream == false)
+                    {
+                        // ? means that the data type is Nullable meaning they might not have a value 
+                        string? lineData = reader.ReadLine();
+                        if (!string.IsNullOrEmpty(lineData))
+                        {
+                            Students.Add(lineData);
+                        }
+                    }
+                }
+                success = true;
+            }
+
+            catch
+            {
+                success = false;
+            }
+
+            return success;
+        }
+
+    }
+}
