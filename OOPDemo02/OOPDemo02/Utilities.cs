f5b037209d3ed72238f50f2e20cdba9fdff80a56
KimJoshua01 <90473470+KimJoshua01@users.noreply.github.com>
2022-01-21 10:10:42 -0700

    Added  OOP Demo 02 and Hockey Team System

---
+++ OOPDemo02/OOPDemo02/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo02
+{   // A static class prevents the creation of an object of the class
+    // Consoloe.WriteLine(), Console.ReadLine()
+    // Math.Round(), Math.Pow()
+    internal class Utilities
+    {
+        public static bool IsValidNameLength(string name, int minLength)
+        {
+            bool isValid = false;
+            if(string.IsNullOrEmpty(name) && name.Length >= minLength)
+            {
+                isValid = true;
+            }
+            return isValid;
+        }
+    }
+}
